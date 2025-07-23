a7ecd950844d7e0bf101388e4a327d7e702949c2
Gabriel Melo <46683068+melogabriel@users.noreply.github.com>
2025-07-23 10:42:52 -0300

    Update check_url.py

--- check_url.py
+++ check_url.py
@@ -149,7 +149,7 @@ def main():
             status = check_generic_url(full_url)
         
         print(f"   - Status: {status}")
-        status_parts.append(f"{title.split(': ')[0]} {status}")
+        status_parts.append(f"{title}': '{status}")
 
     master_data["success"] = "Open NX Shops status list:\n" + "\n".join(status_parts)
 
