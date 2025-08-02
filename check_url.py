cd90c5ef2e4973cc88d055a8cf9f4f5e34beb842
Gabriel Melo <46683068+melogabriel@users.noreply.github.com>
2025-08-02 09:59:19 -0300

    Update check_url.py

--- check_url.py
+++ check_url.py
@@ -151,7 +151,7 @@ def main():
         print(f"   - Status: {status}")
         status_parts.append(f"{title}:  {status}")
 
-    master_data["success"] = "Open NX Shops status list:\n\n" + "\n".join(status_parts)
+    master_data["success"] = "Open NX Shops status list:\n\n" + "\n".join(status_parts) + "\n\nStar on GitHub:https://github.com/OpenNX/opennx.github.io" 
 
     try:
         with open("tinfoil.json", "w", encoding="utf-8") as f:
